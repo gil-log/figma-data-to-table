823abd2a50493220054ca7fd8eeeba0d681aeb04
Gillog <swgil007@naver.com>
2023-09-20 14:53:57 +0900

    Color Table UI

--- code.ts
+++ code.ts
@@ -34,64 +34,63 @@ function getType(value: any): string {
 }
 
 function createTableFromKeys(keys: string[], data: any) {
-  const ROW_HEIGHT = 40;
+  const ROW_HEIGHT = 50;
   const TABLE_WIDTH = 400;
+  const CELL_WIDTH = TABLE_WIDTH / 2;  // 우리가 2개의 셀만 가지고 있기 때문에
+  const TITLE_COLOR: RGB = { r: 0xD9 / 0xFF, g: 0xD9 / 0xFF, b: 0xD9 / 0xFF };
+  const ROW_COLOR: RGB = { r: 0xDB / 0xFF, g: 0xE3 / 0xFF, b: 0x88 / 0xFF };
 
-  // Frame 생성 (컴포넌트로 만들어질 부모 프레임)
   const tableFrame = figma.createFrame();
-  tableFrame.name = 'Table Component';
-  tableFrame.resize(TABLE_WIDTH, (keys.length + 2) * ROW_HEIGHT); // +2 for the title and headers
+  tableFrame.resize(TABLE_WIDTH, (keys.length + 2) * ROW_HEIGHT);  // +2는 제목 행과 전체 테이블의 제목을 위함
+  tableFrame.cornerRadius = 5;
+  tableFrame.fills = [];
+  tableFrame.name = "Table";
 
   // 제목 추가
-  const title = figma.createText();
-  title.fontName = { family: "Inter", style: "Regular" };
-  title.fontSize = 18;
-  title.characters = 'Table';
-  title.x = 10;
-  title.y = 10;
-  tableFrame.appendChild(title);
+  const titleFieldCell = createCell(CELL_WIDTH, ROW_HEIGHT, TITLE_COLOR, 'Field', 16);
+  titleFieldCell.x = 0;
+  titleFieldCell.y = ROW_HEIGHT;
+  tableFrame.appendChild(titleFieldCell);
 
-  // 헤더 추가
-  const headerField = figma.createText();
-  headerField.fontName = { family: "Inter", style: "Regular" };
-  headerField.fontSize = 16;
-  headerField.characters = 'Field';
-  headerField.x = 10;
-  headerField.y = ROW_HEIGHT;
-  tableFrame.appendChild(headerField);
-
-  const headerType = figma.createText();
-  headerType.fontName = { family: "Inter", style: "Regular" };
-  headerType.fontSize = 16;
-  headerType.characters = 'Type';
-  headerType.x = TABLE_WIDTH / 2; // 중간 위치
-  headerType.y = ROW_HEIGHT;
-  tableFrame.appendChild(headerType);
+  const titleTypeCell = createCell(CELL_WIDTH, ROW_HEIGHT, TITLE_COLOR, 'Type', 16);
+  titleTypeCell.x = CELL_WIDTH;
+  titleTypeCell.y = ROW_HEIGHT;
+  tableFrame.appendChild(titleTypeCell);
 
   // 각 키와 그에 해당하는 타입을 행으로 추가
-  keys.forEach((key, index) => {
+  keys.forEach(async (key, index) => {
     const yPosition = (index + 2) * ROW_HEIGHT;
 
-    const keyText = figma.createText();
-    keyText.fontName = { family: "Inter", style: "Regular" };
-    keyText.fontSize = 14;
-    keyText.characters = key;
-    keyText.x = 10;
-    keyText.y = yPosition;
-    tableFrame.appendChild(keyText);
+    const keyCell = createCell(CELL_WIDTH, ROW_HEIGHT, ROW_COLOR, key, 14);
+    keyCell.x = 0;
+    keyCell.y = yPosition;
+    tableFrame.appendChild(keyCell);
 
-    const typeText = figma.createText();
-    typeText.fontName = { family: "Inter", style: "Regular" };
-    typeText.fontSize = 14;
-    typeText.characters = getType(data[key]);
-    typeText.x = TABLE_WIDTH / 2; // 중간 위치
-    typeText.y = yPosition;
-    tableFrame.appendChild(typeText);
+    const typeCell = createCell(CELL_WIDTH, ROW_HEIGHT, ROW_COLOR, await getType(data[key]), 14);
+    typeCell.x = CELL_WIDTH;
+    typeCell.y = yPosition;
+    tableFrame.appendChild(typeCell);
   });
 
-  // 프레임을 figma의 현재 페이지에 추가
   figma.currentPage.appendChild(tableFrame);
-
-  // 사용자가 테이블을 쉽게 볼 수 있게 확대/이동
   figma.viewport.scrollAndZoomIntoView([tableFrame]);
+}
+
+function createCell(width: number, height: number, color: RGB, textValue: string, fontSize: number): FrameNode {
+  const cellFrame = figma.createFrame();
+  cellFrame.resize(width, height);
+  cellFrame.backgrounds = [{ type: 'SOLID', color }];
+  cellFrame.strokes = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
+
+  const cellText = figma.createText();
+  cellText.fontName = { family: "Inter", style: "Regular" };
+  cellText.fontSize = fontSize;
+  cellText.characters = textValue;
+  cellText.resize(width, height);
+  cellText.textAlignHorizontal = 'CENTER';
+  cellText.textAlignVertical = 'CENTER';
+
+  cellFrame.appendChild(cellText);
+
+  return cellFrame;
 }
